98c15827602e05e8f71819889c09b14f2918a91d
Ron Borysovski <roni537@gmail.com>
2018-09-13 15:23:27 +0300

    refactor: Remove unused methods

--- src/video/video.manager.ts
+++ src/video/video.manager.ts
@@ -6,16 +6,8 @@ export class VideoManager implements VideoRepository {
         return VideoRepository.create(video);
     }
 
-    static createMany(videos: IVideo[]) {
-        return VideoRepository.createMany(videos);
-    }
-
     static updateById(id: string, video: Partial<IVideo>) {
         return VideoRepository.updateById(id, video);
-    }
-
-    static updateMany(videoFilter: Partial<IVideo>, video: Partial<IVideo>) {
-        return VideoRepository.updateMany(videoFilter, video);
     }
 
     static deleteById(id: string) {
