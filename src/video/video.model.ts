86fbbb00d19dbfb0b94e89146513fac9f75f8fe6
Ron Borysovski <roni537@gmail.com>
2018-09-16 16:06:14 +0300

    fix: Add validators to schema

--- src/video/video.model.ts
+++ src/video/video.model.ts
@@ -1,25 +1,44 @@
 import * as mongoose from 'mongoose';
 import { IVideo } from './video.interface';
+import { VideoValidatons } from './validator/video.validations';
 
 const videoSchema: mongoose.Schema = new mongoose.Schema(
     {
         title: {
             type: String,
             required: true,
+            validate: (value: string) => {
+                return VideoValidatons.isTitleValid(value);
+            },
         },
         description: String,
         views: Number,
         owner: {
             type: String,
             required: true,
+            validate: {
+                validator: (value: string) => {
+                    return VideoValidatons.isOwnerValid(value);
+                },
+            },
         },
         contentUrl: {
             type: String,
             required: true,
+            validate: {
+                validator: (value: string) => {
+                    return VideoValidatons.isUrlValid(value);
+                },
+            },
         },
         thumbnailUrl: {
             type: String,
             required: true,
+            validate: {
+                validator: (value: string) => {
+                    return VideoValidatons.isUrlValid(value);
+                },
+            },
         },
         status: {
             type: String,
