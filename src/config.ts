8d06f309525b9bfa540ff1b22b2155434d9369c8
Ron Borysovski <roni537@gmail.com>
2018-11-05 11:26:29 +0200

    refactor: Changed configuration layout

--- src/config.ts
+++ src/config.ts
@@ -1,143 +1,39 @@
-export type Configuration = {
-    db: {
-        host: string;
-        name: string;
-        port: number;
-    };
-    logger: {
-        durable: boolean;
-        exchangeType: string;
-        exchange: string;
-        host: string;
-        port: number;
-        password: string;
-        username: string;
-        persistent: boolean;
-    };
-    rabbitMQ: {
-        host: string;
-        exchanges: {
-            videoReceiver: string;
-            videoPublisher: string;
-        };
-        reconnect_timeout: number;
-    };
-    server: {
-        port: number,
-        name: string,
-    };
-    authentication: {
-        required: boolean;
-        secret: string;
-    };
-};
-
-const development: Configuration = {
+export const config = {
     db: {
         host: process.env.DB_SERVER || 'localhost',
         name: process.env.DB_NAME || 'blue-stream-video',
-        port: 27017,
+        port: +(process.env.DB_PORT || 27017),
     },
     logger: {
         durable: false,
-        exchangeType: 'topic' || process.env.RMQ_LOGGER_TYPE,
-        exchange: 'blue_stream_logs' || process.env.RMQ_LOGGER_EXCHANGE,
-        host: 'localhost' || process.env.RMQ_LOGGER_HOST,
-        port: 15672 || process.env.RMQ_LOGGER_PORT,
-        password: 'guest' || process.env.RMQ_LOGGER_PASS,
-        username: 'guest' || process.env.RMQ_LOGGER_USER,
+        exchangeType: process.env.RMQ_LOGGER_TYPE || 'topic',
+        exchange: process.env.RMQ_LOGGER_EXCHANGE || 'blue_stream_logs',
+        host: process.env.RMQ_LOGGER_HOST || 'localhost',
+        port: +(process.env.RMQ_LOGGER_PORT || 15672),
+        username: process.env.RMQ_LOGGER_USER || 'guest',
+        password: process.env.RMQ_LOGGER_PASS || 'guest',
         persistent: false,
     },
     rabbitMQ: {
-        host: 'localhost',
+        host: process.env.RMQ_HOST || 'localhost',
+        port: +(process.env.RMQ_PORT || 5672),
         exchanges: {
             videoReceiver: 'video',
             videoPublisher: 'video',
         },
         reconnect_timeout: 1000,
+        password: process.env.RMQ_PASSWORD || 'guest',
+        username: process.env.RMQ_USERNAME || 'guest',
     },
     server: {
-        port: 3000,
-        name: 'video',
+        port: +(process.env.PORT || 3000),
+        name: process.env.SERVICE_NAME || 'video',
+    },
+    cors: {
+        allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:4200/'],
     },
     authentication: {
         required: true,
-        secret: process.env.SECRET_KEY || 'bLue5tream@2018', // Don't use static value in production! remove from source control!
+        secret: process.env.SECRET_KEY || 'bLue5tream@2018',
     },
 };
-
-const production: Configuration = {
-    db: {
-        host: process.env.DB_SERVER || 'localhost',
-        name: process.env.DB_NAME || 'blue-stream-video',
-        port: 27017,
-    },
-    logger: {
-        durable: false,
-        exchangeType: 'topic' || process.env.RMQ_LOGGER_TYPE,
-        exchange: 'blue_stream_logs' || process.env.RMQ_LOGGER_EXCHANGE,
-        host: 'localhost' || process.env.RMQ_LOGGER_HOST,
-        port: 15672 || process.env.RMQ_LOGGER_PORT,
-        password: 'guest' || process.env.RMQ_LOGGER_PASS,
-        username: 'guest' || process.env.RMQ_LOGGER_USER,
-        persistent: false,
-    },
-    rabbitMQ: {
-        host: 'localhost',
-        exchanges: {
-            videoReceiver: 'video',
-            videoPublisher: 'video',
-        },
-        reconnect_timeout: 1000,
-    },
-    server: {
-        port: process.env.PORT ? +process.env.PORT : 3000,
-        name: 'video',
-    },
-    authentication: {
-        required: true,
-        secret: process.env.SECRET_KEY || 'bLue5tream@2018', // Don't use static value in production! remove from source control!
-    },
-};
-
-const test: Configuration = {
-    db: {
-        host: process.env.DB_SERVER || 'localhost',
-        name: process.env.DB_NAME || 'blue-stream-video-test',
-        port: 27017,
-    },
-    logger: {
-        durable: false,
-        exchangeType: 'topic' || process.env.RMQ_LOGGER_TYPE,
-        exchange: 'blue_stream_logs' || process.env.RMQ_LOGGER_EXCHANGE,
-        host: 'localhost' || process.env.RMQ_LOGGER_HOST,
-        port: 15672 || process.env.RMQ_LOGGER_PORT,
-        password: 'guest' || process.env.RMQ_LOGGER_PASS,
-        username: 'guest' || process.env.RMQ_LOGGER_USER,
-        persistent: false,
-    },
-    rabbitMQ: {
-        host: 'localhost',
-        exchanges: {
-            videoReceiver: 'video',
-            videoPublisher: 'video',
-        },
-        reconnect_timeout: 1000,
-    },
-    server: {
-        port: process.env.PORT ? +process.env.PORT : 3000,
-        name: 'video',
-    },
-    authentication: {
-        required: true,
-        secret: process.env.SECRET_KEY || 'bLue5tream@2018', // Don't use static value in production! remove from source control!
-    },
-};
-
-const configuration: { [index: string]: Configuration } = {
-    development,
-    production,
-    test,
-};
-
-export const config = configuration[process.env.NODE_ENV || 'development'];
