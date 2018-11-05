e0501ff3adf97ed4c3ede4b59b49b69b81b3ecf2
Ron Borysovski <roni537@gmail.com>
2018-11-05 10:30:07 +0200

    refactor: Add min title length and max description length

--- src/video/validator/video.validations.ts
+++ src/video/validator/video.validations.ts
@@ -3,6 +3,8 @@ import { IVideo } from '../video.interface';
 
 export class VideoValidatons {
     private static readonly maxTitleLength = 256;
+    private static readonly minTitleLength = 3;
+    private static readonly maxDescriptionLength = 5000;
     private static readonly userRegex = /\w+@\w+/i;
     private static readonly urlRegex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/i;
 
@@ -20,7 +22,16 @@ export class VideoValidatons {
 
     static isTitleValid(title: string): boolean {
         const trimmed = title ? title.trim() : null;
-        return !!trimmed && trimmed.length < VideoValidatons.maxTitleLength;
+        return (
+            !!trimmed &&
+            trimmed.length <= VideoValidatons.maxTitleLength &&
+            trimmed.length >= VideoValidatons.minTitleLength
+        );
+    }
+
+    static isDescriptionValid(description: string): boolean {
+        const trimmed = description ? description.trim() : '';
+        return trimmed.length <= VideoValidatons.maxDescriptionLength;
     }
 
     static isOwnerValid(owner: string): boolean {
