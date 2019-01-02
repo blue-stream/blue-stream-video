67603324c2204be27eac76c4176509f57e0b20f6
Ron Borysovski <roni537@gmail.com>
2019-01-02 19:35:54 +0200

    refactor: Add views router endpoint

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { VideoRouter } from './video/video.router';
+import { ViewRouter } from './view/view.router';
 
 const AppRouter: Router = Router();
 
 AppRouter.use('/api/video', VideoRouter);
+AppRouter.use('/api/view', ViewRouter);
 
 export { AppRouter };
