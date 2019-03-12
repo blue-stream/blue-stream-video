26d77080c573749449ccce6570c5265755c109fb
Yuval Assa <assa.yuval107@gmail.com>
2019-03-12 17:34:41 +0200

    feat: Add getSearchedUserPps function

--- src/classification/user-pp/user-pp.repository.ts
+++ src/classification/user-pp/user-pp.repository.ts
@@ -1,5 +1,6 @@
 import { IUserPp } from './user-pp.interface';
 import { UserPpModel } from './user-pp.model';
+import { PpModel } from '../pp/pp.model';
 
 export class UserPpRepository {
     static createPps(pps: IUserPp[]): Promise<IUserPp[]> {
@@ -12,5 +13,39 @@ export class UserPpRepository {
 
     static removeUserPps(userId: string): Promise<void> {
         return UserPpModel.remove({ user: userId }).exec();
+    }
+
+    static getSearchedUserPps(userId: string, searchFilter: string = '', isSysAdmin: boolean = false) {
+        if (isSysAdmin) {
+            return PpModel.aggregate([
+                { $match: { name: { $regex: searchFilter, $options: 'i' } } },
+                { $project: { id: '$_id', _id: false, name: true } },
+            ]);
+        }
+
+        return UserPpModel.aggregate([
+            { $match: { user: userId } },
+            {
+                $lookup: {
+                    from: 'pps',
+                    let: { ppId: '$ppId' },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ['$$ppId', '$_id'],
+                                },
+                            },
+                        },
+                        { $match: { name: { $regex: searchFilter, $options: 'i' } } },
+                    ],
+                    as: 'userPps',
+                },
+            },
+            { $unwind: '$userPps' },
+            { $replaceRoot: { newRoot: '$userPps' } },
+            { $project: { id: '$_id', _id: false, name: true } },
+            { $sort: { name: 1 } },
+        ]);
     }
 }
