38c436b05b7c8bef3ba4e9872839c26b47b93562
Ron Borysovski <roni537@gmail.com>
2019-03-17 15:01:07 +0200

    refactor: add doc expiration date

--- src/classification/last-update/classification-last-update.model.ts
+++ src/classification/last-update/classification-last-update.model.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 import { IClassificationLastUpdate } from './classification-last-update.interface';
+import { config } from '../../config';
 
 const classificationLastUpdateSchema: mongoose.Schema = new mongoose.Schema(
     {
@@ -7,12 +8,12 @@ const classificationLastUpdateSchema: mongoose.Schema = new mongoose.Schema(
             type: String,
             required: true,
             unique: true,
-        },
+        }
     },
     {
         versionKey: false,
         timestamps: {
-            createdAt: false,
+            createdAt: 'expireAt',
             updatedAt: 'date',
         },
         toJSON: {
@@ -23,5 +24,8 @@ const classificationLastUpdateSchema: mongoose.Schema = new mongoose.Schema(
         },
     },
 );
+
+// If `config.classification.expirationDays` is changed, mongodb index need to be changed manully.
+classificationLastUpdateSchema.index({ expireAt: 1 }, { expireAfterSeconds: config.classifications.expirationDays * 24 * 60 * 60 });
 
 export const ClassificationLastUpdateModel = mongoose.model<IClassificationLastUpdate & mongoose.Document>('ClassificationLastUpdate', classificationLastUpdateSchema);
