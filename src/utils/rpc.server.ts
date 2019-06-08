aa1c81d545b942c30b5a501ebad0319faea6c141
Almog Vagman Ciprut <almogvc@gmail.com>
2019-06-08 11:55:26 +0300

    feat: add RPC to get videos by ids

--- src/utils/rpc.server.ts
+++ src/utils/rpc.server.ts
@@ -1,4 +1,6 @@
 import { ViewManager } from '../view/view.manager';
+import { VideoManager } from '../video/video.manager';
+import { IVideo } from '../video/video.interface';
 const jayson = require('jayson/promise');
 
 export const RPCServer = new jayson.Server({
@@ -6,5 +8,19 @@ export const RPCServer = new jayson.Server({
         const channelViews = await ViewManager.getChannelsViews(channels);
 
         return channelViews;
+    },
+
+    async getVideos(videosIds: string[], userId: string, isSysAdmin: boolean) {
+        const videos: IVideo[] | null = await VideoManager.getByIds(videosIds, userId, isSysAdmin);
+
+        const videosMap: { [id: string]: IVideo } = {};
+
+        if (videos) {
+            videos.forEach((video: IVideo) => {
+                videosMap[video.id!] = video;
+            });
+        }
+
+        return videosMap;
     },
 });
